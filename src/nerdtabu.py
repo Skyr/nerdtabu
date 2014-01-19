5c1a2d8ca8521bc3e1d969e6b9949849c921539a
Stefan Schlott <stefan@ploing.de>
2014-01-19 20:57:38 +0100

    Game stub, countdown for round

--- src/nerdtabu.py
+++ src/nerdtabu.py
@@ -8,11 +8,19 @@ import pygame
 from settings import Settings
 
 
+screen = None
+
+
+def modify_settings(settings):
+    pass
+
+
 def team_get_ready(settings, current_team):
+    global screen
     font = pygame.font.SysFont("None", 32)
-    settings.screen.fill([0,0,0])
-    settings.screen.blit(font.render(settings.teams[current_team], True, (255,0,0)), (100,100))
-    settings.screen.blit(font.render("Get ready!", True, (255,0,0)), (100,300))
+    screen.fill([0,0,0])
+    screen.blit(font.render(settings.teams[current_team], True, (255,0,0)), (100,100))
+    screen.blit(font.render("Get ready!", True, (255,0,0)), (100,300))
     pygame.display.update()
     run_loop = True
     while run_loop:
@@ -21,28 +29,80 @@ def team_get_ready(settings, current_team):
             if event.type == pygame.QUIT:
                 run_loop = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_s:
+                    modify_settings(settings)
+                elif event.key == pygame.K_SPACE:
                     run_loop = False
 
 
+def display_card(settings, card):
+    # TODO: Display answer and forbidden words
+    pass
+
+
+def play_round(settings, current_team, cards):
+    global screen
+    start_time = time.time() * 1000
+    round_time_left = settings.timeLimit * 1000
+    last_sec_display = -1
+    run_loop = True
+    number_width = settings.number[0].get_width()
+    number_height = settings.number[0].get_height()
+    screen.fill([0,0,0])
+    display_card(settings, cards[0])
+    pygame.display.update()
+    while run_loop:
+        time.sleep(0.01)
+        remaining_time = start_time + round_time_left - time.time() * 1000
+        run_loop = (remaining_time > 0)
+        if (int(remaining_time/1000)!=last_sec_display):
+            # Update time display
+            last_sec_display = int(remaining_time/1000)
+            screen.blit(settings.number[(last_sec_display/10)%10],
+                    (settings.countdown_x, settings.countdown_y))
+            screen.blit(settings.number[last_sec_display%10],
+                    (settings.countdown_x + number_width, settings.countdown_y))
+            pygame.display.update((settings.countdown_x, settings.countdown_y,
+                2*number_width, number_height))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run_loop = False
+            # TODO: Keypress "pause"
+            # TODO: Keypress "correct"
+            # TODO: Keypress "oops"
+            # TODO: Keypress "abort"
+    cards.pop()
+
+
+def show_final_scores(settings):
+    pass
+
+
 def main():
+    global screen
     print("Nerd Tabu")
     # Parse command line
     parser = argparse.ArgumentParser()
     parser.add_argument('quizfile', type=argparse.FileType('r'), help='Name of the quiz file')
     parser.add_argument('teamA', nargs='?', metavar='teamname_A', default='Team A', help='Name of team A')
     parser.add_argument('teamB', nargs='?', metavar='teamname_B', default='Team B', help='Name of team B')
+    parser.add_argument('--datadir', default='.', help='Resource directory')
     args = parser.parse_args()
     # Update settings
-    settings = Settings(args.quizfile)
+    settings = Settings(args.quizfile, args.datadir)
     settings.teams = [ args.teamA, args.teamB ]
     # Initial game data
     cards = settings.get_random_cards()
     current_team = 0
     # Main game loop
     pygame.init()
-    settings.screen = pygame.display.set_mode([1024,768])
-    team_get_ready(settings, current_team)
+    screen = pygame.display.set_mode([1024,768]) # , pygame.FULLSCREEN)
+    settings.load_data()
+    while len(cards)>0:
+        team_get_ready(settings, current_team)
+        play_round(settings, current_team, cards)
+        current_team = (current_team + 1) % len(settings.teams)
+    show_final_scores(settings)
     pygame.quit()
 
 
--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,25 @@
-import yaml
 import random
+
+import yaml
+import pygame
 
 class Settings:
     timeLimit = 45
     teams = [ "Team A", "Team B" ]
+    score = [ 0, 0 ]
     questions = { }
+    number = [ ]
+    countdown_x = 600
+    countdown_y = 500
 
-    def __init__(self, quizfile):
+    def __init__(self, quizfile, datadir):
         self.questions = yaml.safe_load(quizfile)
+        self.datadir = datadir
+
+    def load_data(self):
+        self.number = [ ]
+        for n in range(10):
+            self.number = self.number + [ pygame.image.load("%s/numbers/%d.png" % (self.datadir, n)).convert() ]
 
     def get_random_cards(self):
         cards = self.questions.keys()
