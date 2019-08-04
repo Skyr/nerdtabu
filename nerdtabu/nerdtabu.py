48d700081f9f68b26403543c34adef4ee4fb1aff
Stefan Schlott <stefan@ploing.de>
2019-08-04 18:33:59 +0200

    Wait for space instead of any key

--- nerdtabu/nerdtabu.py
+++ nerdtabu/nerdtabu.py
@@ -38,12 +38,12 @@ def display_scores(theme, settings):
     blit_centered(theme.team_rect[1], [team, score], score.get_height()/4)
 
 
-def wait_for_keypress():
+def wait_for_space_pressed():
     run_loop = True
     while run_loop:
         time.sleep(0.1)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 run_loop = False
 
 
@@ -51,17 +51,17 @@ def show_title(theme):
     global screen
 
     screen.fill([0, 0, 0])
-    press_key = theme.main_font.render("Press key to start", True, pygame.Color(255, 255, 255))
+    press_key = theme.main_font.render("Press space to start", True, pygame.Color(255, 255, 255))
     blit_centered(theme.main_rect, [press_key], press_key.get_height() / 3)
     pygame.display.update()
-    wait_for_keypress()
+    wait_for_space_pressed()
 
     screen.fill([0, 0, 0])
     screen.blit(theme.title_img, (0, 0))
     pygame.display.update()
     if theme.title_sound:
         theme.title_sound.play()
-    wait_for_keypress()
+    wait_for_space_pressed()
     theme.title_sound.fadeout(2000)
 
 
